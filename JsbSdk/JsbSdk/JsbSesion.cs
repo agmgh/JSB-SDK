fa79c4fcc2071ce645ee2d1b6fb5029a2c515fbf
fjch1 <fjch1@LLOYD-PC-XPS15>
2016-07-06 10:08:05 +0800

    it worked

--- JsbSdk/JsbSdk/JsbSesion.cs
+++ JsbSdk/JsbSdk/JsbSesion.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,20 +27,13 @@ namespace JsbSdk
             this.accessKey = accessKey;
             this.secretKey = secretKey;
         }
-        
-        public async Task<string> FetchAsync(Uri uri)
+
+        public async Task<string> FetchAsync(Uri uri, string verb, Dictionary<string, string> parameters = null)
         {
-            if (!uri.IsAbsoluteUri)
-                uri = new Uri(JsbRestBaseUri, uri);
-            string requestId = Guid.NewGuid().ToString();
-            string timeStamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss+0000");
-            var request = WebRequest.CreateHttp(uri);
-            request.Method = "PUT";
-            request.Headers["x-jsb-sdk-req-timestamp"] = timeStamp;
-            request.Headers["x-jsb-sdk-req-uuid"] = requestId;
-            request.Headers[HttpRequestHeader.Authorization] = GetAuthorizationHeader("PUT", uri.ToString(), timeStamp, requestId);
-            
-            using (var response = await request.GetResponseAsync() as HttpWebResponse)
+            if (uri == null)
+                throw new ArgumentNullException("uri");
+
+            using (var response = await Request(uri, verb, parameters))
             using (var stream = response.GetResponseStream())
             using (var reader = new StreamReader(stream, Encoding.UTF8))
             {
@@ -47,25 +41,61 @@ namespace JsbSdk
             }
         }
 
-        public string Fetch(Uri uri)
+        public Task<string> FetchAsync(Uri uri, Dictionary<string, string> parameters = null)
         {
-            return FetchAsync(uri).Result;
+            return FetchAsync(uri, "GET", parameters);
         }
+
+        public string Fetch(Uri uri, string verb, Dictionary<string, string> parameters = null)
+        {
+            return FetchAsync(uri, verb, parameters).Result;
+        }
+
+        public string Fetch(Uri uri, Dictionary<string, string> parameters = null)
+        {
+            return FetchAsync(uri, "GET", parameters).Result;
+        }
+
+        public async Task<HttpWebResponse> Request(Uri uri, string verb, Dictionary<string, string> paramters = null)
+        {
+            if (!uri.IsAbsoluteUri)
+                uri = new Uri(JsbRestBaseUri, uri);
+            verb = verb.ToUpper();
+            string requestId = Guid.NewGuid().ToString();
+            string timeStamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss+0000");
+
+            if (paramters != null)
+            {
+                if (!string.IsNullOrEmpty(uri.Query))
+                    throw new InvalidOperationException("Uri cannnot contain a query when no parameters are specified.");
+                uri = new Uri(uri.ToString() + "?" + string.Join("&", paramters.Select(p => WebUtility.UrlEncode(p.Key) + "=" + WebUtility.UrlEncode(p.Value))));
+            }
+
+            var request = WebRequest.CreateHttp(uri);
+            request.Method = verb;
+            request.Headers["x-jsb-sdk-req-timestamp"] = timeStamp;
+            request.Headers["x-jsb-sdk-req-uuid"] = requestId;
+            request.Headers[HttpRequestHeader.Authorization] = GetAuthorizationHeader("PUT", uri.ToString(), timeStamp, requestId);
+
+            var response = await request.GetResponseAsync() as HttpWebResponse;
+            return response;
+        }
+
 
         private string GetAuthorizationHeader(string method, string uri, string timestamp, string requestId)
         {
             //Build a string containing essential information used to identify a request. It will not be sent to the server but instead, used to generate a digest. The generated digest will be keyed-hashed and sent to the server.
             var stringBuilder = new StringBuilder();
             stringBuilder.Append(method.ToUpper());
-            stringBuilder.Append("\r\n");
+            stringBuilder.Append("\n");
             stringBuilder.Append(WebUtility.UrlEncode(uri).ToLower());
-            stringBuilder.Append("\r\n");
+            stringBuilder.Append("\n");
             stringBuilder.Append("x-jsb-sdk-req-timestamp:");
             stringBuilder.Append(timestamp);
-            stringBuilder.Append("\r\n");
+            stringBuilder.Append("\n");
             stringBuilder.Append("x-jsb-sdk-req-uuid:");
             stringBuilder.Append(requestId.ToLower());
-            stringBuilder.Append("\r\n");
+            stringBuilder.Append("\n");
 
             var stringToSign = stringBuilder.ToString();
             var stringToSignUtf8 = Encoding.UTF8.GetBytes(stringToSign);
@@ -84,7 +114,7 @@ namespace JsbSdk
 
             //Build the authorization header string.
             stringBuilder = new StringBuilder();
-            stringBuilder.Append("Credential=" + this.accessKey);
+            stringBuilder.Append("Credential=" + "1234567");
             stringBuilder.Append(",SignedHeaders=x-jsb-sdk-req-timestamp;x-jsb-sdk-req-uuid,Signature=");
             stringBuilder.Append(signatureHex);
             return stringBuilder.ToString();
--- JsbSdk/JsbSdkTest/UnitTest1.cs
+++ JsbSdk/JsbSdkTest/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace JsbSdkTest
         {
             var jsbWeb = new JsbWeb("asdoooooooooooooooooooooooooohfiohdoassssssssssswwwwwwwwwwwwwwwwww", "1234567");
             var result = jsbWeb.FetchAsync(jsbWeb.SignTestUri).Result;
-            
+            Assert.AreEqual(result, "{\"error_response\":{\"code\":\"success\",\"msg\":\"success\",\"sub_code\":\"\",\"sub_msg\":\"signature match\",\"request_id\":\"\"}}");
         }
     }
 }
